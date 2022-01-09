6fc8ed5d80ea661c1047f3fb4ec63bd3f1d460f6
Edmund Lewry <edmund.lewry@ntlworld.com>
2022-01-09 10:02:20 +0000

    Managed to render just the path and the area that path sits in. Would like it to be more strict with what is clipped out

--- map.js
+++ map.js
@@ -1,25 +1,49 @@
-const background = new ol.layer.Tile({
-  className: 'stamen',
-  source: new ol.source.Stamen({
-    layer: 'toner',
-  }),
-});
-
 const base = new ol.layer.Tile({
   source: new ol.source.OSM(),
 });
 
+
+const raster = new ol.source.Raster({
+  sources: [base],
+  operation: function(pixels, data) {
+    const pixel = pixels[0];
+    pixel[0] = 183;
+    pixel[1] = 183;
+    pixel[2] = 183;
+    pixel[3] = 255;
+
+    return pixel;
+  }
+});
+
+const fog = new ol.layer.Image({
+  source: raster
+});
+
+const pathStyle = new ol.style.Style({
+  stroke: new ol.style.Stroke({
+    color: "#f00",
+    width: 5
+  })
+});
+
 const gpxLayer = new ol.layer.Vector({
   source: new ol.source.Vector({
-    url: "./"
-  });
+    url: "./local-park.gpx",
+    format: new ol.format.GPX(),
+  }),
+  style: function(feature) {
+    return pathStyle;
+  }
 });
 
 const clipLayer = new ol.layer.Vector({
   style: null,
   source: new ol.source.Vector({
-    url: 'data.json',
-    format: new ol.format.GeoJSON(),
+    // url: 'data.json',
+    // format: new ol.format.GeoJSON(),
+    url: "./local-park.gpx",
+    format: new ol.format.GPX(),
   }),
 });
 
@@ -35,22 +59,24 @@ const style = new ol.style.Style({
 });
 
 base.on('postrender', function (e) {
+  var i = 0;
   const vectorContext = ol.render.getVectorContext(e);
   e.context.globalCompositeOperation = 'destination-in';
   clipLayer.getSource().forEachFeature(function (feature) {
-    console.log(feature);
+    console.log(++i);
+    console.log(feature.A);
     vectorContext.drawFeature(feature, style);
   });
   e.context.globalCompositeOperation = 'source-over';
 });
 
 const map = new ol.Map({
-  layers: [background, base],
+  layers: [base, clipLayer, gpxLayer],
   target: 'map',
   view: new ol.View({
-    center: ol.proj.fromLonLat([-0.058898418,51.406058646]),
     // center: ol.proj.fromLonLat([8.23, 46.86]),
     // zoom: 7,
+    center: ol.proj.fromLonLat([-0.058898418,51.406058646]),
     zoom: 14,
   }),
 });
