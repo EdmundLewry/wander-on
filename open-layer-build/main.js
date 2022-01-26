7fffe9e1c452747b48ac8e77fde6c2ca16f1ca59
Edmund Lewry <edmund.lewry@ntlworld.com>
2022-01-26 20:49:26 +0000

    Added drag and drop functionality and began to look into exporting feature data

--- open-layer-build/main.js
+++ open-layer-build/main.js
@@ -3,12 +3,13 @@ import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import {Style, Stroke, Fill} from 'ol/style';
-import GPX from 'ol/format/GPX';
+import {GPX, GeoJSON} from 'ol/format';
 import Vector from 'ol/source/Vector';
 import VectorImageLayer from 'ol/layer/VectorImage';
 import { fromLonLat } from 'ol/proj';
 import Raster from 'ol/source/Raster';
 import ImageLayer from 'ol/layer/Image';
+import { DragAndDrop, defaults as defaultInteractions } from 'ol/interaction';
 
 const base = new TileLayer({
   source: new OSM(),
@@ -17,7 +18,7 @@ const base = new TileLayer({
 const pathStyle = new Style({
   stroke: new Stroke({
     color: "#f00",
-    width: 5,
+    width: 20,
   }),
   fill: new Fill({
     color: "#b7b7b7"
@@ -26,7 +27,7 @@ const pathStyle = new Style({
 
 const gpxFormat = new GPX({extractStyles: false});
 const gpxSource = new Vector({
-  url: "../local-park.gpx",
+  url: "./local-park.gpx",
   format: gpxFormat,
 });
 
@@ -51,11 +52,32 @@ const fog = new ImageLayer({
   source: raster
 });
 
+const fileDropInteraction = new DragAndDrop({
+  formatConstructors: [GPX]
+});
+
+fileDropInteraction.on('addfeatures', function (event) {
+  console.log("Adding new features");
+  gpxSource.addFeatures(event.features);
+});
+
 const map = new Map({
+  interactions: defaultInteractions().extend([fileDropInteraction]),
   layers: [fog],
   target: 'map',
   view: new View({
     center: fromLonLat([-0.058898418,51.406058646]),
-    zoom: 14,
+    zoom: 16,
   }),
 });
+
+const geoJsonFormat = new GeoJSON({extractStyles: false});
+var button = document.getElementById("button");
+button.onclick = function () {
+  console.log("button clicked");
+  gpxSource.forEachFeature( function(feature) {
+    console.log(gpxFormat.writeFeatures([feature]));
+    // console.log(geoJsonFormat.writeFeature(feature));
+    //https://stackoverflow.com/questions/2601745/how-to-convert-vector-layer-coordinates-into-map-latitude-and-longitude-in-openl/2607145#2607145
+  });
+}
