440f6b20c6744be688245f290af72efaa5d85f50
Edmund Lewry <edmund.lewry@ntlworld.com>
2022-04-26 07:55:03 +0100

    Restyling mobile navbar for toggle menu

--- wander-on-application/Frontend/src/app/nav-menu/nav-menu.component.ts
+++ wander-on-application/Frontend/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+  @Output() print: EventEmitter<any> = new EventEmitter();
+
   isExpanded = false;
 
   collapse() {
@@ -14,5 +16,9 @@ export class NavMenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  printFeatures() {
+    this.print.emit();
   }
 }
